aeb4b9a0181534207f65a2a64de2baa2a3004635
Faizan <rfaizan15@gmail.com>
2024-08-12 16:26:08 +0500

    Functionality added. Extra credits added.

--- script.js
+++ script.js
@@ -1,21 +1,46 @@
-let gridLength = 50;
+let gridLength = 16;
 
 function makegrid() {
-  //     const body = document.querySelector("body");
-  // body.removeChild(".gridDiv");
+  const gridLengthDisplay = document.querySelectorAll(".gridLength");
+  gridLengthDisplay.forEach((display) => {
+    display.textContent = gridLength;
+  });
+  const body = document.querySelector("body");
+  const grid = document.querySelector(".gridDiv");
+  body.removeChild(grid);
+  const newGrid = document.createElement("div");
+  newGrid.classList.add("gridDiv");
+  body.appendChild(newGrid);
 
   const yBox = document.querySelector(".gridDiv");
   for (let i = 0; i < gridLength; i++) {
     const newYBox = document.createElement("div");
     for (let j = 0; j < gridLength; j++) {
       const newXBox = document.createElement("div");
-      newXBox.textContent = ".";
       newXBox.classList.add("xBox");
       newYBox.appendChild(newXBox);
     }
     newYBox.classList.add("yBox");
     yBox.appendChild(newYBox);
   }
+
+  const allBoxes = document.querySelectorAll(".xBox");
+  allBoxes.forEach((box) => {
+    box.addEventListener("mouseover", () => {
+        // box.style.backgroundColor = "aquamarine";
+      let r = Math.floor(Math.random() * 256);
+      let g = Math.floor(Math.random() * 256);
+      let b = Math.floor(Math.random() * 256);
+      box.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+      box.style.opacity = "0.6";
+    });
+  });
 }
+
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", () => {
+  gridLength = parseInt(prompt("Enter Length: "));
+  makegrid();
+});
 
 makegrid();
